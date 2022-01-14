db4b9c5f06f3921bdb209604b0b20f3c23236ce0
Deepshikha <deepshikha4862@gmail.com>
2022-01-14 11:42:50 +0530

    frontend 7

--- src/app/bookinglist/bookinglist.component.ts
+++ src/app/bookinglist/bookinglist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-bookinglist',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookinglistComponent implements OnInit {
 
-  constructor() { }
+  x:number[]=[1,2,3];
+  y:number[]=[1,2,3,4,5];
+  str:string="occupied";
+  str1:string="book";
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
 
+  changeStat()
+  {
+    this.str="vacant";
+  }
+
+  transmit()
+{
+  this.router.navigate(['/refund']);
+  this.str1="cancelled";
+}
 }
